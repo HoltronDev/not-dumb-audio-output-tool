dd2be7bf17e18a5ace9e3ad198baa5f973c4b583
Benjamin Holton <ben@holtron.dev>
2022-04-15 10:30:36 -0400

    Fixed icon issue.

--- NotDumbAudioOutputTool/NotDumbAudioOutputTool/resource.h
+++ NotDumbAudioOutputTool/NotDumbAudioOutputTool/resource.h
@@ -5,12 +5,13 @@
 #define IDI_ICON1                       103
 #define IDR_MENU1                       104
 #define IDD_DIALOG1                     105
+#define IDB_PNG1                        107
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
